d899133b0d0773fff50b492c45f89eb93622fa3e
Abel3581 <marzoa3581@gmail.com>
2022-05-24 11:34:16 -0300

    actualizacion

---
+++ src/main/java/fiados/com/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package fiados.com.repository;
+
+import fiados.com.models.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
