bdbef8126e3aac32af8d0cf0b0b75473cddd36e0
Abel3581 <marzoa3581@gmail.com>
2022-06-03 17:16:21 -0300

    agregado busacar por id trade

--- src/main/java/fiados/com/models/response/TradeResponse.java
+++ src/main/java/fiados/com/models/response/TradeResponse.java
@@ -1,12 +1,29 @@
 
 package fiados.com.models.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.Builder;
 
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter @Setter
-@Builder
 public class TradeResponse {
+
+    private Long id;
+    private String email;
+    private String role;
+    private String dni;
+    private String city;
+    private String adress;
+    private String country;
+    private String firstName;
+    private String lastName;
+    private String company_name;
+   private List<BranchResponse> branchResponseList;
+
     
 }
