e9f8f810cb8790de3796b2231844fb1545faef4d
Abel3581 <marzoa3581@gmail.com>
2022-06-15 00:40:51 -0300

    mas filtros

--- src/main/java/fiados/com/repository/UserRepository.java
+++ src/main/java/fiados/com/repository/UserRepository.java
@@ -13,6 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
     @Query(value = "select u from User u where first_name like :term or last_name like :term or role like :term " +
-            "or city like :term or adress like :term or company_name like :term")
+            "or city like :term or adress like :term or company_name like :term or country like :term or dni like :term")
     List<User> search(String term);
 }
